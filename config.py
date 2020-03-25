1c2d69e7d2d4431b99de26d814e18158e63d04c2
eugene <earzamastsev@gmail.com>
2020-03-25 12:47:53 +0500

    v1.0 [DB postrges] - add env DATABASE_URI

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import os
+
 DEBUG = False
 SECRET_KEY = 'my_secret_key'
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
 
-SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:eugene123@127.0.0.1:5432/flask_db"
+# SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:eugene123@127.0.0.1:5432/flask_db"
+
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
