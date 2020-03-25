e91200cec4f55b2c540413e147bd2f9b02570fb7
eugene <earzamastsev@gmail.com>
2020-03-25 12:15:33 +0500

    change DB from sqlite to postrges

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 DEBUG = True
 SECRET_KEY = 'my_secret_key'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+
+SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:eugene123@127.0.0.1:5432/flask_db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
